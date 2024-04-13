208e7b0a8cb68151b2d35a540ce7b04bb15b94f3
makifyegin <mehmetakifyegin@icloud.com>
2024-04-14 00:41:19 +0100

    dev

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// username should be a mandotry field
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+
 
 app.use(cors());
 app.use(express.static("public"));
@@ -10,61 +26,126 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 const { v4: uuidv4 } = require("uuid");
 
+
+app.get('/mongo-health', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
-});
-var users = [];
-app.post("/api/users", (req, res) => {
-  const username1 = req.body.username;
-  const id = uuidv4();
-  const newUser = { username: username1, _id: id };
-  users.push(newUser);
+}
+);
 
-  res.json({ username: username1, _id: id });
-});
+
+app.post("/api/users", (req, res) => {
+  const username = req.body.username;
+  User.findOne({ username
+  }, (err, data) => {
+    if (err) {
+      res.send("Error");
+    } else if (data) {
+      res.send("Username already taken");
+    } else {
+      const newUser = new User({
+        username: username,
+      });
+      newUser.save((err, data) => {
+        if (err) {
+          res.send("Error");
+        } else {
+          res.json({
+            username: data.username,
+            _id: data._id,
+          });
+        }
+      });
+    }
+  });
+} 
+);
+
+
+//Make a get request ti get all the users as an array
 
 app.get("/api/users", (req, res) => {
-  res.json(users);
+  let users= [];
+  User.find({}, (err, data) => {
+    if (err) {
+      res.send("Error");
+    } else {
+      data.forEach((user) => {
+        users.push({
+          username: user.username,
+          _id: user._id,
+        });
+      });
+      res.send(users);
+    }
+  });
 });
 
-app.post("/api/users/:_id/exercises", (req, res) => {
-
-  const { description, duration, date } = req.body;
-  const userId = req.params._id;
-  const user = users.find((user) => user._id === userId);
-  if (!user) {
-    return res.json({ error: "user not found" });
-  }
-  const newExercise = {
-    description: description,
-    duration: parseInt(duration),
-    date: date ? new Date(date).toDateString() : new Date().toDateString(),
-    _id: userId,
-    username: user.username,
-  };
-  res.json(newExercise);
-
-});
+//You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
 
 app.get("/api/users/:_id/logs", (req, res) => {
   const userId = req.params._id;
-  const user = users.find((user) => user._id === userId);
-  if (!user) {
-    return res.json({ error: "user not found" });
-  }
-  const { from, to, limit } = req.query;
-  let log = user.log;
-  if (from) {
-    log = log.filter((exercise) => new Date(exercise.date) >= new Date(from));
-  }
-  if (to) {
-    log = log.filter((exercise) => new Date(exercise.date) <= new Date(to));
-  }
-  if (limit) {
-    log = log.slice(0, limit);
-  }
-  res.json({ ...user, count: log.length, log });
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit;
+  User.findById(userId, (err, data) => {
+    if (err) {
+      res.send("Error");
+    } else {
+      const log = data.log;
+      let result = log;
+      if (from) {
+        result = result.filter((exercise) => new Date(exercise.date) > new Date(from));
+      }
+      if (to) {
+        result = result.filter((exercise) => new Date(exercise.date) < new Date(to));
+      }
+      if (limit) {
+        result = result.slice(0, limit);
+      }
+      res.json({
+        _id: data._id,
+        username: data.username,
+        count: result.length,
+        log: result,
+      });
+    }
+  });
+
 });
+
+//You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const userId = req.params._id;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  let date = req.body.date;
+  if (!date) {
+    date = new Date().toDateString();
+  } else {
+    date = new Date(date).toDateString();
+  }
+  User.findById(userId, (err, data) => {
+    if (err) {
+      res.send("Error");
+    } else {
+      res.json({
+        username: data.username,
+        description: description,
+        duration: duration,
+        _id: data._id,
+        date: date,
+      });
+    }
+  });
+}
+);
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
